e80aecc7e0d09518d4db3607304bcccff09d88b1
ivoivoivo0043 <ivo.n.bjj@gmail.com>
2018-04-28 16:49:26 +0200

    MySql initial setup

--- ToBeDone/Data/ApplicationDbContext.cs
+++ ToBeDone/Data/ApplicationDbContext.cs
@@ -22,5 +22,7 @@ namespace ToBeDone.Data
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
         }
+
+        public DbSet<ToBeDone.Models.Employee> Employee { get; set; }
     }
 }
---
+++ ToBeDone/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToBeDone.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+
+        public string Education { get; set; }
+
+        public string WorkExp { get; set; }
+
+    }
+}
